e48a9301de58b24848a98213229ae5cd5f04668e
SCPgamerscp <168889478+SCPgamerscp@users.noreply.github.com>
2026-06-13 00:32:42 +0900

    Revert "Fix Skeleton King animation bug"

--- src/main/java/com/ecrea/elementmagicboss/client/model/SkeletonKingModel.java
+++ src/main/java/com/ecrea/elementmagicboss/client/model/SkeletonKingModel.java
@@ -8,7 +8,6 @@ public class SkeletonKingModel extends AbstractSpellCastingMobModel {
 
     public static final ResourceLocation MODEL = new ResourceLocation("elementmagicboss", "geo/sans.geo.json");
     public static final ResourceLocation TEXTURE = new ResourceLocation("elementmagicboss", "textures/entity/sans.png");
-    public static final ResourceLocation ANIMATION = new ResourceLocation("elementmagicboss", "animations/sans.animation.json");
 
     @Override
     public ResourceLocation getModelResource(AbstractSpellCastingMob animatable) {
@@ -18,10 +17,5 @@ public class SkeletonKingModel extends AbstractSpellCastingMobModel {
     @Override
     public ResourceLocation getTextureResource(AbstractSpellCastingMob animatable) {
         return TEXTURE;
-    }
-
-    @Override
-    public ResourceLocation getAnimationResource(AbstractSpellCastingMob animatable) {
-        return ANIMATION;
     }
 }
